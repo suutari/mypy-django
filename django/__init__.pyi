8614b7afc04106fdb6c7b2fc0952404f8c05dfc7
Ethan Smith <ethan@ethanhs.me>
2017-06-08 16:50:09 -0700

    correct MultiValueDict, small fixes in Files and Signal

--- django/__init__.pyi
+++ django/__init__.pyi
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, Tuple, Union
 
 # Remove warnings when importing django.db, happens in migrations
 db = None # type: Any
 
 def setup(set_prefix: bool = True) -> None: ...
 
-def get_version(version: Tuple = None) -> str: ...
+def get_version(version: Tuple[Union[int, str]] = None) -> str: ...
