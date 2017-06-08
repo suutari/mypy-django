4a7ef4168a94bb7a3b0b860171dea51088012b5d
Ethan Smith <ethan@ethanhs.me>
2017-06-07 17:08:43 -0700

    add dispatch, signals and signing, improve http attributes

--- django/__init__.pyi
+++ django/__init__.pyi
@@ -2,3 +2,7 @@ from typing import Any
 
 # Remove warnings when importing django.db, happens in migrations
 db = None # type: Any
+
+def setup(set_prefix: bool = True) -> None: ...
+
+def get_version(version: Tuple = None) -> str: ...
