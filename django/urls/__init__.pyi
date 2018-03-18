0d12dd62d62cd61be2d46d37de9d2e3aae796f52
Tuomas Suutari <tuomas@nepnep.net>
2018-03-18 11:06:06 +0200

    Fix exported names of django.urls

--- django/urls/__init__.pyi
+++ django/urls/__init__.pyi
@@ -1,14 +1,33 @@
 from .base import (  # type: ignore  # stub not done yet
-    clear_script_prefix, clear_url_caches, get_script_prefix, get_urlconf,
-    is_valid_path, resolve, reverse, reverse_lazy, set_script_prefix,
-    set_urlconf, translate_url,
+    clear_script_prefix as clear_script_prefix,
+    clear_url_caches as clear_url_caches,
+    get_script_prefix as get_script_prefix,
+    get_urlconf as get_urlconf,
+    is_valid_path as is_valid_path,
+    resolve as resolve,
+    reverse as reverse,
+    reverse_lazy as reverse_lazy,
+    set_script_prefix as set_script_prefix,
+    set_urlconf as set_urlconf,
+    translate_url as translate_url,
 )
-from .exceptions import NoReverseMatch, Resolver404  # type: ignore  # stub not done yet
+from .exceptions import (  # type: ignore  # stub not done yet
+    NoReverseMatch as NoReverseMatch,
+    Resolver404 as Resolver404,
+)
 from .resolvers import (
-    LocaleRegexProvider, LocaleRegexURLResolver, RegexURLPattern,
-    RegexURLResolver, ResolverMatch, get_ns_resolver, get_resolver,
+    LocaleRegexProvider as LocaleRegexProvider,
+    LocaleRegexURLResolver as LocaleRegexURLResolver,
+    RegexURLPattern as RegexURLPattern,
+    RegexURLResolver as RegexURLResolver,
+    ResolverMatch as ResolverMatch,
+    get_ns_resolver as get_ns_resolver,
+    get_resolver as get_resolver,
 )
-from .utils import get_callable, get_mod_func
+from .utils import (
+    get_callable as get_callable,
+    get_mod_func as get_mod_func,
+)
 
 __all__ = [
     'LocaleRegexProvider', 'LocaleRegexURLResolver', 'NoReverseMatch',
