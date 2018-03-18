efbcd8d5600b4880e7c56ae4a1204c2ccf727703
Tuomas Suutari <tuomas@nepnep.net>
2018-03-18 11:02:36 +0200

    Fix exported names of django.http.cookie

--- django/http/cookie.pyi
+++ django/http/cookie.pyi
@@ -1,8 +1,17 @@
 # Stubs for django.http.cookie (Python 3.5)
 
+from http.cookies import Morsel as Morsel
+from http.cookies import SimpleCookie as SimpleCookie
 from typing import Dict
-from http.cookies import SimpleCookie, Morsel
 
 cookie_pickles_properly = ...  # type: bool
 
+
 def parse_cookie(cookie: str) -> Dict[str, str]: ...
+
+
+__all__ = [
+    'Morsel',
+    'parse_cookie',
+    'SimpleCookie',
+]
